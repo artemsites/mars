f322813cad243ee0615a618e72b1629d985daa1a
artemsites <info@artemsites.ru>
2024-01-09 15:28:37 +0400

    fix

--- animations/space.js
+++ animations/space.js
@@ -8,57 +8,58 @@ try {
     initSpaceMouseAnimation();
   });
 
-  window.addEventListener("resize", onEventEndThenStartCallback(250, initSpaceMouseAnimation));
+  window.addEventListener(
+    "resize",
+    onEventEndThenStartCallback(250, initSpaceMouseAnimation)
+  );
 
-    function initSpaceMouseAnimation() {
-      document.removeEventListener("mousemove", animationLogicClasses, false);
-      document.addEventListener("mousemove", animationLogicClasses);
-    }
-    
-    function animationLogicClasses(e) {
-      // Изменения позиции космоса от мыши
-      let viewportWidth =
+  function initSpaceMouseAnimation() {
+    document.removeEventListener("mousemove", animationLogicClasses, false);
+    document.addEventListener("mousemove", animationLogicClasses);
+  }
+
+  let fromRight = false;
+  function animationLogicClasses(e) {
+    // Изменения позиции космоса от мыши
+    let viewportWidth =
       document.body.clientWidth ||
       window.innerWidth ||
       document.documentElement.clientWidth;
 
-      let fromRight = false;
-
-      // center -> left
-      if (e.x >= 0 && e.x < viewportWidth / 3) {
-        elSpace.classList.add("_left");
-        elSpace.classList.remove("_right");
-        elSpace.classList.remove("_center");
-        elSpace.classList.remove("_from-right");
-        fromRight = false;
-      }
-      // center -> rigth
-      else if (e.x <= viewportWidth && e.x > (viewportWidth / 3) * 2) {
-        elSpace.classList.add("_right");
-        elSpace.classList.remove("_left");
-        elSpace.classList.remove("_center");
-        elSpace.classList.remove("_from-right");
-        fromRight = true;
-      }
-      // right -> center
-      else if (
-        fromRight /* && e.x > (viewportWidth/3) && e.x < (viewportWidth/3*2) */
-      ) {
-        elSpace.classList.add("_from-right");
-        elSpace.classList.remove("_left");
-        elSpace.classList.remove("_right");
-        elSpace.classList.remove("_center");
-      }
-      // left -> center
-      else {
-        elSpace.classList.add("_center");
-        elSpace.classList.remove("_left");
-        elSpace.classList.remove("_right");
-        elSpace.classList.remove("_from-right");
-        fromRight = false;
-      }
+    // center -> left
+    if (e.x >= 0 && e.x < viewportWidth / 3) {
+      elSpace.classList.add("_left");
+      elSpace.classList.remove("_right");
+      elSpace.classList.remove("_center");
+      elSpace.classList.remove("_from-right");
+      fromRight = false;
+    }
+    // center -> rigth
+    else if (e.x <= viewportWidth && e.x > (viewportWidth / 3) * 2) {
+      elSpace.classList.add("_right");
+      elSpace.classList.remove("_left");
+      elSpace.classList.remove("_center");
+      elSpace.classList.remove("_from-right");
+      fromRight = true;
+    }
+    // right -> center
+    else if (
+      fromRight /* && e.x > (viewportWidth/3) && e.x < (viewportWidth/3*2) */
+    ) {
+      elSpace.classList.add("_from-right");
+      elSpace.classList.remove("_left");
+      elSpace.classList.remove("_right");
+      elSpace.classList.remove("_center");
+    }
+    // left -> center
+    else {
+      elSpace.classList.add("_center");
+      elSpace.classList.remove("_left");
+      elSpace.classList.remove("_right");
+      elSpace.classList.remove("_from-right");
+      fromRight = false;
+    }
   }
-
 } catch (err) {
   console.error("space.js: ", err);
 } finally {
